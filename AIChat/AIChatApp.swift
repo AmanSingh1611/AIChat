7d6bdfa92a704e342599d397c35b8b475e5fdd61
Aman Singh <aman16024375@gmail.com>
2026-01-05 11:51:57 +0530

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Aman on 05/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

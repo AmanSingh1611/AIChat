81e69c840f91e2bbc22c3403fc956e0e412a1d15
Aman Singh <aman16024375@gmail.com>
2026-01-07 12:45:28 +0530

    [Feature] Added Tabbar and NavigationStacks

---
+++ AIChat/Core/Profile/ProfileView.swift
@@ -0,0 +1,44 @@
+//
+//  ProfileView.swift
+//  AIChat
+//
+//  Created by Aman on 07/01/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @State private var showSettingsView = false
+    
+    var body: some View {
+        NavigationStack {
+            Text("Profile")
+                .navigationTitle("Profile")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        settingsButton
+                    }
+                }
+        }
+        .sheet(isPresented: $showSettingsView) {
+            Text("Settings")
+        }
+    }
+    
+    private var settingsButton: some View {
+        Button {
+            onSettingsButtonPressed()
+        } label: {
+            Image(systemName: "gear")
+                .font(.headline)
+        }
+    }
+    
+    private func onSettingsButtonPressed() {
+        showSettingsView = true
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  AIChat
+//
+//  Created by Aman on 07/01/26.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text("Welcome")
+    }
+}
+
+#Preview {
+    WelcomeView()
+}

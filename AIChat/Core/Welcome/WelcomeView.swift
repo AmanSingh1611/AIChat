6e6af78951371aa63e31c65b29223bf06cad43d2
Aman Singh <amans@advantageclub.ai>
2026-01-19 13:32:52 +0530

    [Feature] Added WelcomeView UI

--- AIChat/Core/Welcome/WelcomeView.swift
+++ AIChat/Core/Welcome/WelcomeView.swift
@@ -8,23 +8,69 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State var randomImage = Constants.randomImage
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Welcome")
-                    .frame(maxHeight: .infinity)
+                ImageLoaderView(urlString: randomImage)
+                    .ignoresSafeArea()
                 
-                NavigationLink {
-                    OnboardingCompletedView()
-                } label: {
-                    Text("Get Started")
-                        .callToActionButton()
-                }
-
+                titleSection
+                    .padding(.top, 26)
+                
+                ctaButtons
+                    .padding(16)
+                
+                policyLinks
             }
-            .padding(16)
         }
     }
+    
+    private var titleSection: some View {
+        VStack(spacing: 8) {
+            Text("AI Chat ")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+        }
+    }
+    
+    private var ctaButtons: some View {
+        VStack(spacing: 8) {
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get Started")
+                    .callToActionButton()
+            }
+            .padding(16)
+            
+            Text("Already have an account? Sign In!")
+                .underline()
+                .font(.body)
+                .padding(10)
+                .tapablebackground()
+                .onTapGesture {
+                    
+                }
+        }
+    }
+    
+    private var policyLinks: some View {
+        HStack(spacing: 8) {
+            Link(destination: URL(string: Constants.termsAndServicesLink)!) {
+                Text("Terms of Service")
+            }
+            
+            Circle()
+                .fill(.accent)
+                .frame(width: 4, height: 4)
+            
+            Link(destination: URL(string: Constants.privacyPolicyLink)!) {
+                Text("Privacy Policy")
+            }
+        }
+    }
+    
 }
 
 #Preview {
---
+++ AIChat/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  AIChat
+//
+//  Created by Aman on 19/01/26.
+//
+
+struct Constants {
+    static let randomImage = String("https://picsum.photos/600/600")
+    static let termsAndServicesLink = String("www.google.com")
+    static let privacyPolicyLink = String("www.google.com")
+}

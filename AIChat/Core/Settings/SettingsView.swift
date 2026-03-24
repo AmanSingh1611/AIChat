932ce304b7bad0220d391b3a6c438515a0fb8857
Aman Singh <aman16024375@gmail.com>
2026-03-24 11:38:53 +0530

    [Feature] Settings View and Create Accounts View

--- AIChat/Core/Settings/SettingsView.swift
+++ AIChat/Core/Settings/SettingsView.swift
@@ -6,26 +6,140 @@
 //
 
 import SwiftUI
+import SwiftfulUtilities
 
 struct SettingsView: View {
+    @Environment(\.dismiss) private var dismiss
     @Environment(AppState.self) private var appState
+    
+    @State private var isPremium: Bool = true
+    @State private var isAnonymousUser = false
+    @State private var showCreateAccountView = false
     
     var body: some View {
         NavigationStack {
             List {
-                Button {
-                    onSignOutButtonPressed()
-                } label: {
-                    Text("Sign out")
-                }
-
+                accountSection
+                purchaseSection
+                applicationSection
             }
             .navigationTitle("Settings")
+            .sheet(isPresented: $showCreateAccountView) {
+                CreateAccountView()
+                    .presentationDetents([.medium])
+            }
+        }
+    }
+    
+    private var accountSection: some View {
+        Section {
+            if isAnonymousUser {
+                Text("Save & back-up account")
+                    .rowFormatting()
+                    .anyButton(.highlight) {
+                        onCreateAccountPressed()
+                    }
+                    .removeListRowFormatting()
+                   
+            } else {
+                Text("Sign Out")
+                    .rowFormatting()
+                    .anyButton(.highlight) {
+                        onSignOutButtonPressed()
+                    }
+                    .removeListRowFormatting()
+            }
+            
+            Text("Delete account")
+                .foregroundStyle(.red)
+                .rowFormatting()
+                .anyButton(.highlight) {
+                    onSignOutButtonPressed()
+                }
+                .removeListRowFormatting()
+        } header: {
+            Text("Account")
+        }
+    }
+    
+    private var purchaseSection: some View {
+        Section {
+            HStack(spacing: 8) {
+                Text("Account Status: \(isPremium ? "PREMIUM" : "FREE")")
+                
+                Spacer(minLength: 0)
+                
+                if isPremium {
+                    Text("MANAGE")
+                        .badgeButton()
+                }
+            }
+            .rowFormatting()
+            .anyButton(.highlight, action: {
+                
+            })
+            .disabled(!isPremium)
+            .removeListRowFormatting()
+        } header: {
+            Text("Purchase")
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section {
+            HStack(spacing: 8) {
+                Text("App Version")
+                Spacer(minLength: 0)
+                Text(Utilities.appVersion ?? "")
+                    .foregroundStyle(.secondary)
+            }
+            .rowFormatting()
+            .removeListRowFormatting()
+            
+            HStack(spacing: 8) {
+                Text("Build Number")
+                Spacer(minLength: 0)
+                Text(Utilities.buildNumber ?? "")
+                    .foregroundStyle(.secondary)
+            }
+            .rowFormatting()
+            .removeListRowFormatting()
+            
+            Text("Contact Us")
+                .foregroundStyle(.blue)
+                .rowFormatting()
+                .anyButton(.highlight) {
+                    
+                }
+                .removeListRowFormatting()
+        } header: {
+            Text("Application")
+        } footer: {
+            Text("Created by Aman Singh")
         }
     }
     
     func onSignOutButtonPressed() {
-        appState.updateViewState(showTabBarView: false)
+        dismiss()
+        Task {
+            try? await Task.sleep(for: .seconds(1))
+            appState.updateViewState(showTabBarView: false)
+        }
+    }
+    
+    func onCreateAccountPressed() {
+        showCreateAccountView = true
+    }
+}
+
+fileprivate
+extension View {
+    func rowFormatting() -> some View {
+        self
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 12)
+            .padding(.vertical, 16)
+            .background(Color(uiColor: .systemBackground))
     }
 }
 
